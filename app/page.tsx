0984da993d63d53c401ed8604070c9b0998f65d0
bidishapandit03 <72354120+bidishapandit03@users.noreply.github.com>
2026-08-23 14:40:53 +0530

    Project skeleton: Next.js 15 with TypeScript and Tailwind, homepage stripped to one line

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <p className="text-lg">Zomato Lite is alive.</p>
+    </main>
+  );
+}
